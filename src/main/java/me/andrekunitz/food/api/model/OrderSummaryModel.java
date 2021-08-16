57615db2452086db707fb44d38c6a105e9f8e754
André Kunitz <andrekunitz@gmail.com>
2021-08-16 18:14:43 -0300

    Filter JSON response payload fields

--- src/main/java/me/andrekunitz/food/api/model/OrderSummaryModel.java
+++ src/main/java/me/andrekunitz/food/api/model/OrderSummaryModel.java
@@ -3,9 +3,12 @@ package me.andrekunitz.food.api.model;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonFilter("orderFilter")
 @Setter
 @Getter
 public class OrderSummaryModel {
