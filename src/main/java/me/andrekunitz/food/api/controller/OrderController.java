43bced353b12631781ef54075dd4c5523e8498d3
André Kunitz <andrekunitz@gmail.com>
2021-08-18 19:55:03 -0300

    Add Squiggly for response filtering

--- src/main/java/me/andrekunitz/food/api/controller/OrderController.java
+++ src/main/java/me/andrekunitz/food/api/controller/OrderController.java
@@ -2,27 +2,24 @@ package me.andrekunitz.food.api.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import lombok.RequiredArgsConstructor;
 import me.andrekunitz.food.api.assembler.OrderInputDisassembler;
 import me.andrekunitz.food.api.assembler.OrderModelAssembler;
 import me.andrekunitz.food.api.assembler.OrderSummaryModelAssembler;
 import me.andrekunitz.food.api.model.OrderModel;
+import me.andrekunitz.food.api.model.OrderSummaryModel;
 import me.andrekunitz.food.api.model.input.OrderInput;
 import me.andrekunitz.food.domain.exception.BusinessException;
 import me.andrekunitz.food.domain.exception.EntityNotFoundException;
@@ -42,21 +39,9 @@ public class OrderController {
 	private final OrderInputDisassembler orderInputDisassembler;
 
 	@GetMapping
-	public MappingJacksonValue list(@RequestParam(required = false) String fields) {
-		var orders = orderRepository.findAll();
-		var ordersSummaryModel = orderSummaryModelAssembler.toCollectionModel(orders);
-
-		MappingJacksonValue ordersWrapper = new MappingJacksonValue(ordersSummaryModel);
-		var filterProvider = new SimpleFilterProvider();
-		filterProvider.addFilter("orderFilter", SimpleBeanPropertyFilter.serializeAll());
-
-		if (StringUtils.isNotBlank(fields)) {
-			filterProvider.addFilter("orderFilter", SimpleBeanPropertyFilter.filterOutAllExcept(fields.split(",")));
-		}
-
-		ordersWrapper.setFilters(filterProvider);
-
-		return ordersWrapper;
+	public List<OrderSummaryModel> list() {
+		return orderSummaryModelAssembler.toCollectionModel(
+				orderRepository.findAll());
 	}
 
 	@GetMapping("{orderCode}")
--- src/main/java/me/andrekunitz/food/api/model/OrderSummaryModel.java
+++ src/main/java/me/andrekunitz/food/api/model/OrderSummaryModel.java
@@ -3,12 +3,9 @@ package me.andrekunitz.food.api.model;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
-
 import lombok.Getter;
 import lombok.Setter;
 
-@JsonFilter("orderFilter")
 @Setter
 @Getter
 public class OrderSummaryModel {
